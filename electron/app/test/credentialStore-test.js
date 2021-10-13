b5711df69e8a19af4a52729ac5648d04241e8514
Robert Patrick <robert.patrick@oracle.com>
2021-10-13 10:48:00 -0500

    cleaning up unit tests and enabling eslint on them

--- electron/app/test/credentialStore-test.js
+++ electron/app/test/credentialStore-test.js
@@ -4,120 +4,125 @@
  * Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
  * @ignore
  */
-"use strict";
+'use strict';
+
 const proxyquire = require('proxyquire');
 const { assert, expect } = require('chai');
+const { before, describe, it } = require('mocha');
 const osUtils = require('../js/osUtils');
 
-const wktLoggerMock = {
-  getLogger: () => {
-    return console;
+/* global process */
+describe('Credential Store tests', () => {
+  const wktLoggerMock = {
+    getLogger: () => {
+      return console;
+    }
+  };
+
+  const CredentialStore = proxyquire('../js/credentialStore', { './wktLogging': wktLoggerMock });
+  let credStore;
+
+  let skipTestsInJenkins = false;
+  if (osUtils.isMac() && 'JENKINS_NODE_COOKIE' in process.env) {
+    skipTestsInJenkins = true;
   }
-}
 
-const CredentialStore = proxyquire('../js/credentialStore', { './wktLogging': wktLoggerMock });
-let credStore;
+  before(async () => {
+    credStore = new CredentialStore('wktui-test');
+    await cleanStaleCredentials();
+  });
 
-let skipTestsInJenkins = false;
-if (osUtils.isMac() && 'JENKINS_NODE_COOKIE' in process.env) {
-  skipTestsInJenkins = true;
-}
+  async function cleanStaleCredentials() {
+    const staleCreds = credStore.findCredentials();
+    if (staleCreds && Array.isArray(staleCreds)) {
+      for (const staleCred of staleCreds) {
+        await credStore.deleteCredential(staleCred.account);
+      }
+    }
+  }
 
-before(async () => {
-  credStore = new CredentialStore('wktui-test');
-  await cleanStaleCredentials();
+  if (!skipTestsInJenkins) {
+    it('store new credential works', async () => {
+      try {
+        await credStore.storeCredential('test1', 'f00b@r!');
+      } catch (err) {
+        assert.fail(`Failed to store credential test1: ${err}`);
+      }
+    });
+
+    // On the Jenkins MacOS slaves, this test fails at least half the time with the error:
+    // The specified item already exists in the keychain.  This causes some of the subsequent
+    // tests to fail.
+    //
+    // This has never once reproduced on a developer's MacOS machine so we will just not run
+    // the tests in the Jenkins MacOS environment.
+    //
+    it('replace existing credential works', async () => {
+      try {
+        await credStore.storeCredential('test1', '8e7rty9wrfhofhd98@92382093ue09udjo8dfsyu');
+        const newCredentialValue = await credStore.getCredential('test1');
+        expect(newCredentialValue).to.equal('8e7rty9wrfhofhd98@92382093ue09udjo8dfsyu');
+      } catch (err) {
+        assert.fail(`Failed to replace existing credential test1: ${err}`);
+      }
+    });
+
+    it('get existing credential works', async () => {
+      try {
+        const credentialValue = await credStore.getCredential('test1');
+        expect(credentialValue).to.equal('8e7rty9wrfhofhd98@92382093ue09udjo8dfsyu');
+      } catch (err) {
+        assert.fail(`Failed to get existing credential test1: ${err}`);
+      }
+    });
+
+    it('get non-existent credential works as expected', async () => {
+      try {
+        const credentialValue = await credStore.getCredential('test2');
+        expect(credentialValue).to.be.null;
+      } catch (err) {
+        assert.fail(`Failed to get non-existent credential test2: ${err}`);
+      }
+    });
+
+    it('find credentials to work', async () => {
+      try {
+        const credentials = await credStore.findCredentials();
+        expect(credentials).to.not.be.null;
+        expect(credentials).to.have.length(1);
+        expect(credentials[0].name).to.equal('test1');
+        expect(credentials[0].value).to.equal('8e7rty9wrfhofhd98@92382093ue09udjo8dfsyu');
+      } catch (err) {
+        assert.fail(`Failed to get credentials: ${err}`);
+      }
+    });
+
+    it('delete non-existent credential works as expected', async () => {
+      try {
+        const result = await credStore.deleteCredential('test2');
+        expect(result).to.be.false;
+      } catch (err) {
+        assert.fail(`Failed to handle delete for non-existent credential: ${err}`);
+      }
+    });
+
+    it('delete existing credential works as expected', async () => {
+      try {
+        const result = await credStore.deleteCredential('test1');
+        expect(result).to.be.true;
+      } catch (err) {
+        assert.fail(`Failed to handle delete for existing credential: ${err}`);
+      }
+    });
+
+    it('find credentials when there are none works as expected', async () => {
+      try {
+        const credentials = await credStore.findCredentials();
+        expect(credentials).to.not.be.null;
+        expect(credentials).to.have.length(0);
+      } catch (err) {
+        assert.fail(`Failed to get credentials when there were none: ${err}`);
+      }
+    });
+  }
 });
-
-async function cleanStaleCredentials() {
-  const staleCreds = credStore.findCredentials();
-  if (staleCreds && Array.isArray(staleCreds)) {
-    for (const staleCred of staleCreds) {
-      await credStore.deleteCredential(staleCred.account);
-    }
-  }
-}
-
-if (!skipTestsInJenkins) {
-  it('store new credential works', async () => {
-    try {
-      await credStore.storeCredential('test1', 'f00b@r!');
-    } catch (err) {
-      assert.fail(`Failed to store credential test1: ${err}`);
-    }
-  });
-
-  // On the Jenkins MacOS slaves, this test fails at least half the time with the error:
-  // The specified item already exists in the keychain.  This causes some of the subsequent
-  // tests to fail.
-  //
-  // This has never once reproduced on a developer's MacOS machine so we will just not run
-  // the tests in the Jenkins MacOS environment.
-  //
-  it('replace existing credential works', async () => {
-    try {
-      await credStore.storeCredential('test1', '8e7rty9wrfhofhd98@92382093ue09udjo8dfsyu');
-      const newCredentialValue = await credStore.getCredential('test1');
-      expect(newCredentialValue).to.equal('8e7rty9wrfhofhd98@92382093ue09udjo8dfsyu');
-    } catch (err) {
-      assert.fail(`Failed to replace existing credential test1: ${err}`);
-    }
-  });
-
-  it('get existing credential works', async () => {
-    try {
-      const credentialValue = await credStore.getCredential('test1');
-      expect(credentialValue).to.equal('8e7rty9wrfhofhd98@92382093ue09udjo8dfsyu');
-    } catch (err) {
-      assert.fail(`Failed to get existing credential test1: ${err}`);
-    }
-  });
-
-  it('get non-existent credential works as expected', async () => {
-    try {
-      const credentialValue = await credStore.getCredential('test2');
-      expect(credentialValue).to.be.null;
-    } catch (err) {
-      assert.fail(`Failed to get non-existent credential test2: ${err}`);
-    }
-  });
-
-  it('find credentials to work', async () => {
-    try {
-      const credentials = await credStore.findCredentials();
-      expect(credentials).to.not.be.null;
-      expect(credentials).to.have.length(1);
-      expect(credentials[0].name).to.equal('test1');
-      expect(credentials[0].value).to.equal('8e7rty9wrfhofhd98@92382093ue09udjo8dfsyu');
-    } catch (err) {
-      assert.fail(`Failed to get credentials: ${err}`);
-    }
-  });
-
-  it('delete non-existent credential works as expected', async () => {
-    try {
-      const result = await credStore.deleteCredential('test2');
-      expect(result).to.be.false;
-    } catch (err) {
-      assert.fail(`Failed to handle delete for non-existent credential: ${err}`);
-    }
-  });
-
-  it('delete existing credential works as expected', async () => {
-    try {
-      const result = await credStore.deleteCredential('test1');
-      expect(result).to.be.true;
-    } catch (err) {
-      assert.fail(`Failed to handle delete for existing credential: ${err}`);
-    }
-  });
-
-  it('find credentials when there are none works as expected', async () => {
-    try {
-      const credentials = await credStore.findCredentials();
-      expect(credentials).to.not.be.null;
-      expect(credentials).to.have.length(0);
-    } catch (err) {
-      assert.fail(`Failed to get credentials when there were none: ${err}`);
-    }
-  });
-}
--- electron/app/test/jsonPath-test.js
+++ electron/app/test/jsonPath-test.js
@@ -4,87 +4,92 @@
  * Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
  * @ignore
  */
-"use strict";
+'use strict';
+
 const { expect } = require('chai');
+const { describe, it } = require('mocha');
+
 const JSONPath = require('../js/jsonPath');
 
-class InputData {
-  constructor() {
-    this.inputData = {
-      dummy: 'myData',
-      image: {
-        baseImagePullRequiresAuthentication: true,
-        baseImagePullUsername: 'scott',
-        oracleSupportUserName: 'fred.flintstone@slaterockandgravel.com'
-      },
-      k8sDomain: {
-        secrets: [
-          {
-            name: 'wilma',
-            username: 'wilma.flintstone@housewives.com'
-          },
-          {
-            name: 'barney',
-            username: 'barney.rubble@slaterockandgravel.com'
-          },
-          {
-            name: 'betty',
-            username: 'betty.rubble@housewives.com'
-          }
-        ]
-      }
-    };
+describe('JSON Path tests', () => {
+  class InputData {
+    constructor() {
+      this.inputData = {
+        dummy: 'myData',
+        image: {
+          baseImagePullRequiresAuthentication: true,
+          baseImagePullUsername: 'scott',
+          oracleSupportUserName: 'fred.flintstone@slaterockandgravel.com'
+        },
+        k8sDomain: {
+          secrets: [
+            {
+              name: 'wilma',
+              username: 'wilma.flintstone@housewives.com'
+            },
+            {
+              name: 'barney',
+              username: 'barney.rubble@slaterockandgravel.com'
+            },
+            {
+              name: 'betty',
+              username: 'betty.rubble@housewives.com'
+            }
+          ]
+        }
+      };
+    }
+
+    getInputData() {
+      return this.inputData;
+    }
   }
 
-  getInputData() {
-    return this.inputData;
-  }
-}
+  it('verify that simple json path works', () => {
+    const inputData = new InputData().getInputData();
+    const jsonPath = 'image.oracleSupportUserName';
 
-it('verify that simple json path works', () => {
-  const inputData = new InputData().getInputData();
-  const jsonPath = 'image.oracleSupportUserName';
+    const result = JSONPath.getJsonPathReference(jsonPath, inputData);
+    expect(result).to.not.be.null;
+    expect(result.reference).to.not.be.null;
+    expect(result.field).to.not.be.null;
 
-  const result = JSONPath.getJsonPathReference(jsonPath, inputData);
-  expect(result).to.not.be.null;
-  expect(result.reference).to.not.be.null;
-  expect(result.field).to.not.be.null;
+    expect(result.reference[result.field]).to.not.be.null;
+    expect(result.reference[result.field]).to.equal(inputData.image.oracleSupportUserName);
 
-  expect(result.reference[result.field]).to.not.be.null;
-  expect(result.reference[result.field]).to.equal(inputData.image.oracleSupportUserName);
+    result.reference[result.field] = null;
+    expect(inputData.image.oracleSupportUserName).to.be.null;
+  });
 
-  result.reference[result.field] = null;
-  expect(inputData.image.oracleSupportUserName).to.be.null;
-});
+  it('verify that a single element json path works', () => {
+    const inputData = new InputData().getInputData();
+    const jsonPath = 'dummy';
 
-it('verify that a single element json path works', () => {
-  const inputData = new InputData().getInputData();
-  const jsonPath = 'dummy';
+    const result = JSONPath.getJsonPathReference(jsonPath, inputData);
+    expect(result).to.not.be.null;
+    expect(result.reference).to.not.be.null;
+    expect(result.field).to.not.be.null;
 
-  const result = JSONPath.getJsonPathReference(jsonPath, inputData);
-  expect(result).to.not.be.null;
-  expect(result.reference).to.not.be.null;
-  expect(result.field).to.not.be.null;
+    expect(result.reference[result.field]).to.not.be.null;
+    expect(result.reference[result.field]).to.equal(inputData.dummy);
 
-  expect(result.reference[result.field]).to.not.be.null;
-  expect(result.reference[result.field]).to.equal(inputData.dummy);
+    result.reference[result.field] = null;
+    expect(inputData.dummy).to.be.null;
+  });
 
-  result.reference[result.field] = null;
-  expect(inputData.dummy).to.be.null;
-});
+  it('verify that a json path with an array works', () => {
+    const inputData = new InputData().getInputData();
+    const jsonPath = 'k8sDomain.secrets[1].username';
 
-it('verify that a json path with an array works', () => {
-  const inputData = new InputData().getInputData();
-  const jsonPath = 'k8sDomain.secrets[1].username';
+    const result = JSONPath.getJsonPathReference(jsonPath, inputData);
+    expect(result).to.not.be.null;
+    expect(result.reference).to.not.be.null;
+    expect(result.field).to.not.be.null;
 
-  const result = JSONPath.getJsonPathReference(jsonPath, inputData);
-  expect(result).to.not.be.null;
-  expect(result.reference).to.not.be.null;
-  expect(result.field).to.not.be.null;
+    expect(result.reference[result.field]).to.not.be.null;
+    expect(result.reference[result.field]).to.equal(inputData.k8sDomain.secrets[1].username);
 
-  expect(result.reference[result.field]).to.not.be.null;
-  expect(result.reference[result.field]).to.equal(inputData.k8sDomain.secrets[1].username);
-
-  result.reference[result.field] = null;
-  expect(inputData.k8sDomain.secrets[1].username).to.be.null;
+    result.reference[result.field] = null;
+    expect(inputData.k8sDomain.secrets[1].username).to.be.null;
+  });
 });
