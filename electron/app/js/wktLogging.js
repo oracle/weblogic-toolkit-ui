167f8762172ec83f631322516b77cd05838b4bad
Robert Patrick <robert.patrick@oracle.com>
2022-03-17 16:38:53 -0500

    updating copyrights

--- electron/app/js/wktLogging.js
+++ electron/app/js/wktLogging.js
@@ -1,3 +1,8 @@
+/**
+ * @license
+ * Copyright (c) 2021, 2022, Oracle and/or its affiliates.
+ * Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+ */
 const path = require('path');
 const winston = require('winston');
 require('winston-daily-rotate-file');
