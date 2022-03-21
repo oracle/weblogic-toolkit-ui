09974d13c73d21bef783c115a100d2b2ab8f5f17
Robert Patrick <31662131+robertpatrick@users.noreply.github.com>
2022-03-21 10:57:39 -0500

    upgrading electron, jet, and other packages (#118)

--- webui/scripts/hooks/before_watch.js
+++ webui/scripts/hooks/before_watch.js
@@ -1,10 +1,9 @@
 /**
-  Copyright (c) 2015, 2021, Oracle and/or its affiliates.
+  Copyright (c) 2015, 2022, Oracle and/or its affiliates.
   Licensed under The Universal Permissive License (UPL), Version 1.0
   as shown at https://oss.oracle.com/licenses/upl/
 
 */
-
 'use strict';
 
 module.exports = function (configObj) {
