ff43c67727e834de6dec772ea054aa3351cc4905
Robert Patrick <robert.patrick@oracle.com>
2023-01-31 13:28:26 -0600

    fixing build issue and updating copyright

--- electron/build/notarize.js
+++ electron/build/notarize.js
@@ -1,9 +1,9 @@
 /**
  * @license
- * Copyright (c) 2021, 2022, Oracle and/or its affiliates.
+ * Copyright (c) 2021, 2023, Oracle and/or its affiliates.
  * Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
  */
-const { notarize } = require('electron-notarize');
+const { notarize } = require('@electron/notarize');
 const path = require('path');
 const fs = require('fs');
 const jsYaml = require('js-yaml');
