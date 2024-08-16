5ae667fff40c592677e061ed35c30fa750cb01f7
Richard Killen <richard.killen@oracle.com>
2024-08-16 13:04:37 -0500

    Improve labeling and type detection for fields

--- webui/src/js/viewModels/modelEdit/edit-field.js
+++ webui/src/js/viewModels/modelEdit/edit-field.js
@@ -6,11 +6,11 @@
 'use strict';
 
 define(['accUtils', 'knockout', 'utils/i18n', 'utils/dialog-helper','ojs/ojarraydataprovider',
-  'utils/modelEdit/model-edit-helper',
+  'utils/modelEdit/model-edit-helper', 'utils/wkt-logger',
   'oj-c/button', 'oj-c/input-text', 'oj-c/input-password'
 ],
 function(accUtils, ko, i18n, DialogHelper, ArrayDataProvider,
-  ModelEditHelper) {
+  ModelEditHelper, WktLogger) {
   function EditField(args) {
     const field = args.field;
     const labelPrefix = args.labelPrefix;
@@ -37,6 +37,18 @@ function(accUtils, ko, i18n, DialogHelper, ArrayDataProvider,
 
     this.fieldLabelMapper = (labelId, payload) => {
       return i18n.t(`model-edit-field-${labelId}`, payload);
+    };
+
+    this.attributeLabel = field => {
+      const key = `${labelPrefix}-attribute-${field.key}-label`;
+      const label = i18n.t(key);
+      return (label === key) ? field.attribute : label;
+    };
+
+    this.attributeHelp = field => {
+      const key = `${labelPrefix}-attribute-${field.key}-help`;
+      const help = i18n.t(key);
+      return (help === key) ? null : help;
     };
 
     this.variableName = ko.computed(() => {
@@ -108,34 +120,44 @@ function(accUtils, ko, i18n, DialogHelper, ArrayDataProvider,
     };
 
     function getDisplayType(field) {
-      if(field.type === 'password') {
+      let fieldType = field.type;
+
+      // some alias attributes have wlst_type like ${offline:online},
+      // in this case use the first value
+      const result = fieldType.match(/^\$\{(.*):(.*)}$/);
+      if(result && (result.length > 1)) {
+        fieldType = result[1];
+      }
+
+      if(fieldType === 'password') {
         return 'password';
       }
 
-      if(field.type === 'boolean') {
+      if(fieldType === 'boolean') {
         return 'boolean';
       }
 
-      if(field.type === 'dict') {
+      if(['dict', 'properties'].includes(fieldType)) {
         return 'dict';
       }
 
-      if(field.type === 'list') {
+      if(['list', 'jarray'].includes(fieldType) || fieldType.startsWith('delimited_string')) {
         return 'list';
       }
 
-      if(field.options) {
+      if(field['options']) {
         return 'choice';
       }
 
-      if(['integer', 'long'].includes(field.type)) {
+      if(['integer', 'long'].includes(fieldType)) {
         return 'integer';
       }
 
-      if(['string', 'credential'].includes(field.type)) {
+      if(['string', 'credential'].includes(fieldType)) {
         return 'string';
       }
 
+      WktLogger.error(`Unrecognized field type '${field.type}' for field ${field.attribute}`);
       return 'unknown';
     }
   }
