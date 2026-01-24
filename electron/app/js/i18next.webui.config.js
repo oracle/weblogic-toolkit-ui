f1950a7fc4dfd011e7dea687c62502b35215562f
Robert Patrick <robert.patrick@oracle.com>
2026-01-24 16:12:53 -0600

    updating copyrights to reflect correct dates

--- electron/app/js/i18next.webui.config.js
+++ electron/app/js/i18next.webui.config.js
@@ -1,3 +1,8 @@
+/**
+ * @license
+ * Copyright (c) 2021, 2025, Oracle and/or its affiliates.
+ * Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+ */
 const i18n = require('i18next');
 const i18nextBackend = require('i18next-fs-backend');
 const path = require('path');
